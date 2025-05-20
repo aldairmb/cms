6dba22d594abac3349c1b9626d3d033ef93e7a0f
aldairmb <145483780+aldairmb@users.noreply.github.com>
2025-05-19 19:16:22 -0600

    week-4

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit {
+
+  documents: Document[] = [
+    new Document('1', 'CIT 260 - Object Oriented Programming', 'Learn object-oriented concepts.', '#', []),
+    new Document('2', 'CIT 366 - Full Web Stack Development', 'Master frontend and backend skills.', '#', []),
+    new Document('3', 'CIT 425 - Data Warehousing', 'Explore data warehouse design and use.', '#', []),
+    new Document('4', 'CIT 460 - Enterprise Development', 'Develop enterprise-level apps.', '#', []),
+    new Document('5', 'CIT 495 - Senior Practicum', 'Capstone project experience.', '#', [])
+  ];
+
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
